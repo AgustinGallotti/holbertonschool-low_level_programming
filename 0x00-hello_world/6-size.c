5026ae47f54991eee5660276a407521ca855f726
AgustinGallotti <AgustinGallotti23@gmail.com>
2022-02-03 11:03:04 -0800

    commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,15 +6,15 @@
  * Return: Always 0 (Success)
  */
 int main() {
-  int intType;
-  float floatType;
-  double doubleType;
   char charType;
+  int intType;
+  double doubleType;
+  float floatType;
 
+  printf("Size of char: %zu bytes\n", sizeof(charType));
   printf("Size of int: %zu bytes\n", sizeof(intType));
-  printf("Size of float: %zu bytes\n", sizeof(floatType));
   printf("Size of double: %zu bytes\n", sizeof(doubleType));
-  printf("Size of char: %zu byte\n", sizeof(charType));
+  printf("Size of float: %zu byte\n", sizeof(floatType));
 
   return 0;
 }
