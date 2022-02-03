9f5b85323a773b7ed741bd6bd6b47194c5b071e9
AgustinGallotti <AgustinGallotti23@gmail.com>
2022-02-03 10:54:56 -0800

    commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,5 +4,12 @@ int main() {
   float floatType;
   double doubleType;
   char charType;
-  return (0);
+
+  // sizeof evaluates the size of a variable
+  printf("Size of int: %zu vytes\n", sizeof(intType));
+  printf("Size of float: %zu bytes\n", sizeof(floatType));
+  printf("Size of double: %zu bytes\n", sizeof(doubleType));
+  printf("Size of char: %zu byte\n", sizeof(charType));
+
+  return 0;
 }
