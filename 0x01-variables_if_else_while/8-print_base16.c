31151f417fa6a9cc48a2a4e7e27769d1f81e552f
AgustinGallotti <AgustinGallotti23@gmail.com>
2022-02-04 12:24:04 -0800

    commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,12 @@
 int main(void)
 {
 	char ch;
+	int num;
 
-	for (ch = '0'; ch = '9'; ch++)
+	for (num = '0'; num = '9'; num++)
 	for (ch = 'a'; ch = 'z'; ch++)
 	{
-		putchar(num);
+		putchar(num ; ch);
 	}
 	putchar('\n');
 	return (0);
