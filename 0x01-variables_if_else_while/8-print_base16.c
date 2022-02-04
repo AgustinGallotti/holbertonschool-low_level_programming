ee6eec1c5af626a389bf1f43012a40cea4ee05a1
AgustinGallotti <AgustinGallotti23@gmail.com>
2022-02-04 12:26:42 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,12 +9,14 @@
 int main(void)
 {
 	char ch;
-	int num;
 
-	for (num = '0'; num = '9'; num++)
-	for (ch = 'a'; ch = 'z'; ch++)
+    for (ch = '0'; ch = '9'; ch++)
 	{
-		putchar(num ; ch);
+		putchar(ch);
+	}
+	for (ch = 'a'; ch = 'f'; ch++)
+	{
+		putchar(ch);
 	}
 	putchar('\n');
 	return (0);
