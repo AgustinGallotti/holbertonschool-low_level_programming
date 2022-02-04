c9f7e14f4c703d9af4ca0d428ab072b0b7de6272
AgustinGallotti <AgustinGallotti23@gmail.com>
2022-02-04 12:20:07 -0800

    commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Entry Point
+*
+* Return: Always 0 (Success)
+*
+*/
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num = 'f'; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
