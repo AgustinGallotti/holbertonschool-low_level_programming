f8cc3e3ee170772d45664ad21b3cd94075222c07
AgustinGallotti <AgustinGallotti23@gmail.com>
2022-02-04 08:28:31 -0800

    commit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry Point
+*
+* Return: 0 (Success)
+*
+*/
+int main(void)
+{
+		int n;
+
+			srand(time(0));
+				n = rand() - RAND_MAX / 2;
+					{
+					if (n > 5)
+						(n / 5);
+					return (n);
+					}
+
+					int lastDigit(int n)
+					{
+						return (n % 10);
+					}
+					int n = 98562;
+					cout << firstDigit(n) << " "
+					     << lastDigit(n) << endl;
+					return (0);
+                    }
+}
+© 2022 GitH
