7c38ffccf14812bfd4bd83d834d217a53d82c347
AgustinGallotti <AgustinGallotti23@gmail.com>
2022-02-04 11:38:14 -0800

    commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Entry Point
+*
+* Return: Always 0 (Success)
+*
+*/
+int main(void)
+{
+int num;
+
+for (num = '0'; num <= 'z'; num++)
+{
+putchar(num);
+}
+putchar('\n');
+return (0);
+}
