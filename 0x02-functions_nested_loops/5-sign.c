e41f849340e5503023d3352172057a2266c3558c
AgustinGallotti <AgustinGallotti23@gmail.com>
2022-02-07 12:46:31 -0800

    commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,16 +4,16 @@
 /**
 * print_sign - check if its uppercase or not
 * @n: fe
-* Return: 1 if its upercase, 0 if otherwise
+* Return: 1 if its +, 1 if its -, 0 if otherwise
 */
 int print_sign(int n)
 {
-if ('n' > 0)
+if (n > 0)
 {
 _putchar ('+');
 return (1);
 }
-else if ('n' = 0)
+else if (n = 0)
 {
 _putchar ('0');
 return (0);
