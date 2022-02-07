393155b39b343a69b45a3c9a45b4b91cf2935eb8
AgustinGallotti <AgustinGallotti23@gmail.com>
2022-02-07 13:26:29 -0800

    commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_last_digit - digittttttttit
+* @a: stststs
+* Return: lastDigit
+*/
+int print_last_digit(int a)
+{
+int lastDigit(int a)
+{
+a = a % 10;
+if (a <= 0)
+{
+a *= -1;
+}
+return a;
+}
+}
