8667d489dfad8607e2d03835b2d380214a095b9a
AgustinGallotti <AgustinGallotti23@gmail.com>
2022-02-07 08:27:44 -0800

    commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+include <stdio.h>
+
+/**
+*
+* main - Entry Point
+*
+* Return: Always 0 (Success)
+*
+*/
+int main(void)
+{
+void print_alphabet_x10(void)
+int (ch)
+
+printf("Print all uppercase letters followed by all lowercase letters on the next line :\n");
+	/* print lowercase letters */
+	for (ch= "a"; ch<= "z"; ch++)
+		putchar(ch);
+		putchar (‘\n’) ;
+		getch();
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype>
+
+/**
+*
+* main - Entry Point
+*
+* Return: Always 0 (Success)
+*/
+int main()
+{
+int _islower(int c);
+char (Ch);
+	   
+printf("\n Please Enter any alphabet\n");
+scanf("%c", &Ch);
+
+if ( islower(Ch) )
+{  
+printf ("\n Entered character is lowercase alphabet");
+}
+else
+{
+printf("\n Entered character is Not lowercase alphabet");
+}
+
+else if 
+	return ((1 = islower(Ch));
+
+else
+	return (0);
+}
