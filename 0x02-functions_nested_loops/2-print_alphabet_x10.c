a34380313aa5da0dbadc0a804e45159612243662
AgustinGallotti <AgustinGallotti23@gmail.com>
2022-02-07 11:01:56 -0800

    commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,13 +7,13 @@ void print_alphabet_x10(void)
 {
 int letter, count;
 count = 0;
-while (count < 10)
-{
-for (letter = 'a'; letter <= 'z'; letter++)
-{
-_putchar(letter);
-}
-count++;
-_putchar('\n');
-}
+	while (count < 10)
+	{
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+	_putchar(letter);
+	}
+	count++;
+	_putchar('\n');
+	}
 }
