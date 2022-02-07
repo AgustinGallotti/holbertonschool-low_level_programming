1856c0619506d14e35b64f036575e15e4c81b654
AgustinGallotti <AgustinGallotti23@gmail.com>
2022-02-07 11:11:06 -0800

    commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -16,10 +16,11 @@ char (ch);
 printf("\n Please Enter any alphabet\n");
 scanf("%c", &ch);
 
-if ( islower(ch) )
+if (islower(ch));
 {  
 printf ("\n Entered character is lowercase alphabet");
 }
+
 else
 {
 printf("\n Entered character is Not lowercase alphabet");
