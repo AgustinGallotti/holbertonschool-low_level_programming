febdc362ac74b41299cfa213d4e6bfb0ccadec11
AgustinGallotti <AgustinGallotti23@gmail.com>
2022-02-07 13:59:41 -0800

    commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,5 +5,5 @@ int main(void)
 {
 time_t t = time(NULL);
 struct tm tm = *localtime(&t);
-printf("now: %d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+_putchar("now: %d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
 }
