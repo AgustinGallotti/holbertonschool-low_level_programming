276c87b1642f97c76cd96221226d53981bfe0f82
AgustinGallotti <AgustinGallotti23@gmail.com>
2022-02-07 14:47:06 -0800

    commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,7 +5,7 @@ int jack_bauer(void)
 	int h, m;
 	for (h = 0, h < 24, h++)
 	{
-		for (m = 0; m < 59; m++)
+		for (m = 0; m < 60; m++)
 		{
 			_putchar((h / 10) + '0');
 			_putchar((h % 10) + '0');
