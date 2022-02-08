c8afc1dde87a72bee6ff82e35d3b3539003140c4
AgustinGallotti <AgustinGallotti23@gmail.com>
2022-02-07 17:03:21 -0800

    commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,39 +7,40 @@
 void times_table(void)
 {
 	int a, b, res;
-
-	a = 0;
-	b = 0;
-	res = 0;
-for (a = 0; a < 10; a++)
-{
-	for (b = 0; b < 10; b++)
+		a = 0;
+		b = 0;
+		res = 0;
+	for (a = 0; a < 10; a++)
 	{
-		res = b * a;
-		if (b != 0)
+		for (b = 0; b < 10; b++)
 		{
-			_putchar (' ');
+			res = b * a;
+			if (b != 0)
+			{
+				_putchar(' ');
 
-		if (res < 10)
-
-			_putchar (' ');
+				if (res < 10)
+				{
+					_putchar(' ');
+				}
+			}
+			if (res > 9)
+			{
+				_putchar((res / 10) + '0');
+				_putchar((res % 10) + '0');
+			}
+			else
+			{
+				_putchar(res + '0');
+			}
+			if (b == 9)
+			{
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(',');
+			}
 		}
-		}
-		if (res > 9)
-		{
-			_putchar((res / 10) + '0');
-			_putchar((res % 10) + '0');
-		}
-
-		else
-		{
-			_putchar(res + '0');
-		}
-		if (b == 9)
-		{
-			_putchar('\n');
-		}
-		else
-			_putchar (',');
 	}
 }
