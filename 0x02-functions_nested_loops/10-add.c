03b92bb711a3f4885cc1d184f28ed5e2900eacff
AgustinGallotti <AgustinGallotti23@gmail.com>
2022-02-07 17:33:20 -0800

    commit

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,21 +1,12 @@
-#include<stdio.h>
 #include "main.h"
 
-int main()
+/**
+*add - asaaa
+*@b: aee
+*@a: hfj
+* Return:fg
+*/
+int add(int a, int b)
 {
-int num1, num2, res;
-
-printf("\nEnter the two numbers : ");
-scanf("%d %d", &num1, &num2);
-res = sum(num1, num2);
-
-printf("nAddition of two number is : ");
-return (0);
-}
-
-int sum(int num1, int num2)
-{
-int num3;
-num3 = num1 + num2;
-return (num3);
+	return (a + b);
 }
