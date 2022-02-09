4529bab5965e07e71b77b6a74c505646d3747eba
AgustinGallotti <AgustinGallotti23@gmail.com>
2022-02-09 14:15:13 -0800

    commit

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_square - as
+* @size: sd
+*
+* Return: msdks
+*/
+void print_square(int size)
+{
+int x, s;
+
+	for (s = 0; s < size; s++)
+	{
+		for (x = 0; x < size; x++)
+			_putchar('*');
+	}
+		_putchar('\n');
+}
