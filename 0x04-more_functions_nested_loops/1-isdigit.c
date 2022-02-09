39f57c0c4bc4f32c38662df914495ff7d65e9933
AgustinGallotti <AgustinGallotti23@gmail.com>
2022-02-09 06:57:35 -0800

    commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+* _isdigit -a function that chekcs for number
+* @c: single letter input
+* Return: 1 if int c is number, 0 else number
+*/
+int _isdigit(int c)
+{
+if (_isdigit(c))
+return (1);
+
+else
+return (0);
+}
