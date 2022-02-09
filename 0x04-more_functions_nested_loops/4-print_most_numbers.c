db42965dea4ed06fff69750d3088b7f03e40a98d
AgustinGallotti <AgustinGallotti23@gmail.com>
2022-02-09 10:15:57 -0800

    commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include "main.h"
 
-/** 
+/**
+* print_most_numbers - a function to print most numbers
 * Return: Always 0 (Success)
 */
 void print_most_numbers(void)
 {
 	char ch;
 
-	for (ch = 48; ch <= 57; ch++);
+	for (ch = 48; ch <= 57; ch++)
 	{
-		if (ch != 50 && ch != 52);
+		if (ch != 50 && ch != 52)
 		_putchar(ch);
 	}
-		_puthcar(ch);
-		return (0);
+	_putchar('\n');
 }
