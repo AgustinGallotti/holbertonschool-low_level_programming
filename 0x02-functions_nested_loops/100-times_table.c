bb0302c61e1bf761c8927cd3730439f452ad04dc
AgustinGallotti <AgustinGallotti23@gmail.com>
2022-02-08 16:08:47 -0800

    commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,7 +17,7 @@ for (mult = 1; mult <= n; mult++)
 _putchar(',');
 _putchar(' ');
 prod = num * mult;
-}
+
 if (prod <= 99)
 _putchar(' ');
 if (prod <= 9)
@@ -27,12 +27,13 @@ if (prod >= 100)
 _putchar((prod / 100) + '0');
 _putchar(((prod / 10)) % 10 + '0');
 }
-else if (prod <= 00 && prod >= 10)
-{
-_putchar((prod / 10) + '0');
-_putchar((prod % 10) + '0');
-}
-_putchar('\n');
-}
-}
-}
+	else if (prod <= 99 && prod >= 10)
+	{
+	_putchar((prod / 10) + '0');
+	}
+	_putchar((prod % 10) + '0');
+	}
+	_putchar('\n');
+	}
+	}
+	}
