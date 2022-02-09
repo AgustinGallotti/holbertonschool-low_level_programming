042593548b6ce98fb74a12c4b879bfac15673692
AgustinGallotti <AgustinGallotti23@gmail.com>
2022-02-09 09:01:28 -0800

    commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,11 +2,11 @@
 #include <ctype.h>
 
 /**
-* _islower -a function that chekcs for lowercase character
+* _isupper -a function that chekcs for upper character
 * @c: single letter input
-* Return: 1 if int c is lowercase, 0 if otherwise
+* Return: 1 if int c is upper, 0 if otherwise
 */
-int _islower(int c)
+int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
 		return (1);
