48c92e4650f335d8442ebf6eaef406854b129daa
AgustinGallotti <AgustinGallotti23@gmail.com>
2022-02-09 09:20:20 -0800

    commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_numbers - abcd
+*/
+void print_numbers(void)
+{
+	int n;
+	for (n = '0' ; n <= '9');
+		putchar(n);
+		putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,3 +2,4 @@ int _putchat(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+int _putchar(putchar)
