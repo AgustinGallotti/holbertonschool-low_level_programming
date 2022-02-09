d765c2d7d43b2cb9ffaf0e0ae6aab6062420c4cb
AgustinGallotti <AgustinGallotti23@gmail.com>
2022-02-09 12:36:01 -0800

    commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,16 +9,16 @@
 */
 void print_diagonal(int n)
 {
-	int space, dis;
+	int x, s;
 
 	if (n >= 0)
 	{
-		for (space = 0; space < dis; space++)
+		for (x = 0; x < n; x++)
 		{
-			for (dis = 0; dis < space; dis++)
+			for (s = 0; s < x; s++)
 				_putchar(' ');
 			_putchar('\\');
-			if (space == n - 1)
+			if (x == n - 1)
 
 				continue;
 
