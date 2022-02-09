1b488ba3c25f03d766f5e87728b4e0e0085e6016
AgustinGallotti <AgustinGallotti23@gmail.com>
2022-02-09 12:09:34 -0800

    commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_diagonal - as
+* @n: sd
+*
+* Return: msdks
+*/
+void print_diagonal(int n)
+{
+int count;
+
+	if (n >= 0)
+	{
+		for (count = 0; count < n; count++)
+		{
+			_putchar(' ');
+			_putchar('\');
+			_putchar('\n');
+		}
+	}
+}
