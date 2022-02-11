d467af5f155fb86de85d440ee044c565a43015e1
AgustinGallotti <AgustinGallotti23@gmail.com>
2022-02-11 12:37:49 -0800

    commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+/**
+* rev_string - char *s
+* _puts - h
+* @s: a
+* Return: algo
+*/
+void rev_string(char *s)
+{
+	int pos = 0;
+	int i = 0;
+
+	while ((s[pos] != '\0')
+		pos++;
+
+	for (i = pos -1; i >= 0; i--)
+		_putchar(s[i]);
+
+		_putchar('\n');
+}
