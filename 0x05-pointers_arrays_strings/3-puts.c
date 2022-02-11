0e165b7b140dc2249d556ea2632d94dbd06b9f8d
AgustinGallotti <AgustinGallotti23@gmail.com>
2022-02-11 11:49:42 -0800

    commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,15 +4,14 @@
 * _puts - instrucciones
 * @str: whilee
 */
-
 void _puts(char *str)
 {
 	int i = 0;
-	
-	while (*(str + i) != 0);
+
+	while (*(str + i) != 0)
 	{
-	_putchar(*(str + i));
-	i++;
+		_putchar(*(str + i));
+		i++;
 	}
 	_putchar('\n');
 }
