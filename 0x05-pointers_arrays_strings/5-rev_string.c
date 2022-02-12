e3457094e2f78f199cf5acd86515f2b3f5a6cbd0
AgustinGallotti <AgustinGallotti23@gmail.com>
2022-02-11 17:24:13 -0800

    commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,19 +1,25 @@
 #include "main.h"
-#include <string.h>
 /**
-* rev_string - char *s
-* @s: a
-* Return: algo
-*/
+ *rev_string - asd
+ *@s: asd
+ *
+ */
 void rev_string(char *s)
 {
 	int pos = 0;
 	int i = 0;
+	char sup;
 
 	while (s[pos] != '\0')
+	{
 		pos++;
+		sup++;
+	}
 
-	for (i = pos - 1; i >= 0; i--)
-
-	_putchar('\n');
+	for (i = pos - 1; i >= pos / 2; i--)
+	{
+		sup = s[i];
+		s[i] = s[pos - i - 1];
+		s[pos - i - 1] = sup;
+	}
 }
