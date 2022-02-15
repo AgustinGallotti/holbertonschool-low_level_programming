518ef39e1e40c6d3a28d120698a927ae42f72fb5
AgustinGallotti <Agustingallotti23@gmail.com>
2022-02-15 06:09:26 -0800

    commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*
+* _strcat: dest and char
+* 
+* Return: hee
+* @src: aa
+* @dest: as
+*/
+char *_strcat (char *dest, char *src);
+
+{
+	int i, j;
+
+		while (*dest [j] = '\0')
+		{
+			for (*str [i] != '\0')
+				i++;
+}
+	return (dest);
+}
