f71404679605a96db4b1dbd3f805a8617f0d2846
AgustinGallotti <Agustingallotti23@gmail.com>
2022-02-15 08:56:38 -0800

    commit

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+char *
+strncpy(char *dest, const char *src, size_t n)
+{
+	while (dest [a])
+		a++
+	
+	while (b < n && src [b])
+	{	
+		dest[a] = src[b]
+	}
+	return dest;
+}
