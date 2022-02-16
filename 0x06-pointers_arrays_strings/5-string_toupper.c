0a3171f568166401599ecf3774598a731c41a66b
AgustinGallotti <Agustingallotti23@gmail.com>
2022-02-16 05:11:10 -0800

    commit

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,1 +1,21 @@
-s
+#include "main.h"
+
+/**
+* string_toupper - changes
+* @t: puntero
+* Return: el contenido
+*/
+char *string_toupper(char *t)
+{
+	int x = 0;
+
+	while (t[x] != '\0')
+	{
+		x++;
+	}
+	if (t[x] >= 97 && t[x] <= 122)
+	{
+		t[x] -= 32;
+	}
+	return (t);
+}
