fdc931965770dde06e9decda533ea4c57a9c5897
AgustinGallotti <Agustingallotti23@gmail.com>
2022-02-17 04:41:19 -0800

    commit

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,21 +1,18 @@
 #include "main.h"
-
 /**
-* string_toupper - changes
-* @t: puntero
-* Return: el contenido
+* string_toupper -   function that changes all lowercase to uppercase
+* @a: pointer
+* Return: a
 */
-char *string_toupper(char *t)
-{
-	int x = 0;
 
-	while (t[x] != '\0')
+char *string_toupper(char *a)
+{
+	int x;
+
+	for (x = 0; a[x] != '\0'; x++)
 	{
-		x++;
+		if (a[x] >= 97 && a[x] <= 122)
+			a[x] -= 32;
 	}
-	if (t[x] >= 97 && t[x] <= 122)
-	{
-		t[x] -= 32;
-	}
-	return (t);
+	return (a);
 }
