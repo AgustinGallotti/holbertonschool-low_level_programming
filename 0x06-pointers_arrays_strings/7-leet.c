92975c522dd217b9305804fdca9bd3b1f5ce6534
AgustinGallotti <Agustingallotti23@gmail.com>
2022-02-17 04:26:52 -0800

    commit

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * char *leet(cha *) - replace
+ * @str: string assigned
+ * Return: replace
+ */
+char *leet(char *str)
+{
+	char SP[6] = {4, 3, 0, 7, 1};
+	char x[6] = {a, e, o, t, l};
+	char j[6] = {A, E, O, T, L};
+
+	while (str[x] > 64 && str[x] < 91; i++)
+	{
+		x = SP
+	{
+		while (str[j] > 96 && j[SP] < 123; j++)
+		{
+			j = SP
+		}
+			if (str[x] == SP[x])
+			{
+				str[x] 
+			}
+	}
+return (str);
+}
