b2930bb7712e49da0c861b982ef52364baeb2d03
AgustinGallotti <Agustingallotti23@gmail.com>
2022-02-17 06:19:43 -0800

    commit

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,1 +1,27 @@
-s
+#include "main.h"
+
+/**
+* print_number - mix
+* @n: as
+* return: putchar
+*/
+void print_number(int)
+
+	unsigned int dc, dig, nat = n;
+	double x = 1;
+	
+	if (n == 0)
+	{
+		n < 0; n = nat * -1;
+		_putchar('-');
+	}
+	while (x <= nat)
+		x *= 10;
+	dc = x / 10;
+
+	while (dc >= 1)
+	{
+		dig = nat / dc;
+		_putchar(dig + '0');
+		nat = (nat - (dc * dig));
+		dc /= 10;
