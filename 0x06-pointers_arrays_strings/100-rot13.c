e13169c5a31b62f40ed745bcd49556bb47a99f6e
AgustinGallotti <Agustingallotti23@gmail.com>
2022-02-17 05:04:26 -0800

    commit

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,1 +1,27 @@
-s
+#include "main.h"
+
+/**
+ * leet - a function that encodes a string into 1337
+ *@a: pointer
+ *Return: 1337
+ */
+char *rot13(char *a)
+{
+	char abc[] = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwxyz";
+	char rot[] ="NOPQRSTUVWXYZabcdefghijklmopqrstuvwxyzABCDEFGHIJKLM";
+	int x;
+	int z;
+
+	for (x = 0; a[x] != '\0'; x++)
+	{
+		for (z = 0; z < 6; z++)
+		{
+			if (a[x] == abc[z])
+			{
+				abc[x] = rot[z];
+				break;
+			}
+		}
+	}
+return (a);
+}
