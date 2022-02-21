4206c2c09d9b7e3d99a52949c630e602fdc65c87
AgustinGallotti <Agustingallotti23@gmail.com>
2022-02-21 07:48:41 -0800

    commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _strchr - character
+* @s: se
+* @c: cc
+* Return: null or c
+*/
+char *_strchr(char *s, char c)
+{
+	int a, b;
+
+	for (a = 0; s[a] != c; a++)
+	{
+	s[a] = c;
+	return (c);
+	}
+		for (b = 0; s[b] = '\0';)
+			return (0);
+}
