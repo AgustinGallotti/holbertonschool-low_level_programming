861224387e44e0595c46decd0d7a700e4677b56a
AgustinGallotti <Agustingallotti23@gmail.com>
2022-02-22 04:12:38 -0800

    commit

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,3 +2,4 @@ void set_string(char **s, char *to);
 char *_memset(char *s, char b, unsigned int n);
 unsigned int _strspn(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
+int _putchar(char c);
