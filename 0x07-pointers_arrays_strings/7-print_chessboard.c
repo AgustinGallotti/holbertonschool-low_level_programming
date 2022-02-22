4c890edabbef2df0d6fbb78fbf0eafe638412082
AgustinGallotti <Agustingallotti23@gmail.com>
2022-02-22 04:00:09 -0800

    commit

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*print_chessboard - literally that
+*@a: pointer to array
+*
+*/
+void print_chesboard(char (*a)[8])
+{
+	int i, j;
+	
+	for (i = 0; i < 8; j++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar (a[i][j]);
+		_putchar('\n');
+		}
+	}
+}
