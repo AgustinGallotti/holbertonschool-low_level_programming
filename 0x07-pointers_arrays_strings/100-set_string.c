b07ccbf12f9a4a02855a3b83924de718e3c4a49d
AgustinGallotti <Agustingallotti23@gmail.com>
2022-02-22 04:10:15 -0800

    commit

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+*set_string - cambiar
+*@s
+*@to
+*return: value
+*/
+void set_string(char **s, char *to);
+
+	*(s + to);
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
+void set_string(char **s, char *to);
 char *_memset(char *s, char b, unsigned int n);
 unsigned int _strspn(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
