a73cbceb9a8398b682c3a1652b6ceb120a5c769b
AgustinGallotti <Agustingallotti23@gmail.com>
2022-02-21 17:28:57 -0800

    commit

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,15 +8,16 @@
 */
 unsigned int _strspn(char *s, char *accept)
 {
-	int a, b, c;
+	int a, b, c = 0;
 
-	for (a = 0 ;s[a] != '\0'; a++)
+	for (a = 0 ; s[a] != ' '; a++)
 		for (b = 0; accept[b] != '\0'; b++)
-			if (s[a] == accept[b])
-			{
-				c++;
-				break;
-			}
-
+	{
+		if (s[a] == accept[b])
+		{
+			c++;
+			break;
+		}
+	}
 	return (c);
 }
