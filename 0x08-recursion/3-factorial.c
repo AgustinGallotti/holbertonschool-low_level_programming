313ffcfb733245e8b992962232ff983cd0ae8859
AgustinGallotti <Agustingallotti23@gmail.com>
2022-02-23 10:54:33 -0800

    commit

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
 * factorial - facotrization
 * @n: variable
@@ -8,7 +7,12 @@ int factorial(int n)
 {
 	if (n < 0)
 	{
-		return (0);
+		return (-1);
 	}
-	return (n * factorial(n - 1));
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+		return (n * factorial(n - 1));
 }
