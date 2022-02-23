f0b2e12c5e56fba668b67147469299f129dbf922
AgustinGallotti <Agustingallotti23@gmail.com>
2022-02-23 13:02:13 -0800

    commit

--- 0x08-recursion/5-main.c
+++ 0x08-recursion/5-main.c
@@ -1,19 +1,26 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-* _pow_recursion - a function that returns the value of x raised to y
-* @x: siuu
-* @y: siuuuuu
-* Return: value
-*/
-int _sqrt_recursion(int n)
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-	if (n < 0)
-	{
-		return (-1);
-	}
-	else if (n == 0)
-	}
-	else
-	return (x * _pow_recursion(x, y - 1));
+    int r;
+
+    r = _sqrt_recursion(1);
+    printf("%d\n", r);
+    r = _sqrt_recursion(1024);
+    printf("%d\n", r);
+    r = _sqrt_recursion(16);
+    printf("%d\n", r);
+    r = _sqrt_recursion(17);
+    printf("%d\n", r);
+    r = _sqrt_recursion(25);
+    printf("%d\n", r);
+    r = _sqrt_recursion(-1);
+    printf("%d\n", r);
+    return (0);
 }
