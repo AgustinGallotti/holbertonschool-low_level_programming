a1823cf6a2b721c5f8a220be89d02921e449ca19
AgustinGallotti <Agustingallotti23@gmail.com>
2022-02-25 05:01:30 -0800

    commit

---
+++ 0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* argc - is an argument
+* argv - vector de c
+* return: name
+*/
+int main (int argc, char* argv[])
+{
+		printf ("argc = %d\n", argc);
+		return (0);
+}
