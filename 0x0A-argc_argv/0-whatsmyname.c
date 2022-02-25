9374ae469def54d4a33783a6c691e8743ca1135f
AgustinGallotti <Agustingallotti23@gmail.com>
2022-02-25 11:51:07 -0800

    commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
-* int main - argumentos
+* main - argumentos
 * @argc: argument
 * @argv: puntero
 * Return: 0
 */
 int main(int argc, char *argv[])
 {
-	{
 	printf("%s\n", argv[argc - 1]);
 	return (0);
 }
