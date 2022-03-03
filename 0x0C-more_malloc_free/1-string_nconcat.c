c14ed6c24aaa8d5bc54e4f9b011d2552013e8bca
AgustinGallotti <Agustingallotti23@gmail.com>
2022-03-03 09:07:43 -0800

    commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,45 +1,45 @@
 #include "main.h"
 #include <stdlib.h>
 /**
-* _strlen - lenght
-* @p: puntero
-* Return: i
+* _strlen - calculate the lenght
+* @str: string
+* Return: string
 */
-int _strlen(char *p)
+int _strlen(char *str)
 {
 	int i = 0;
 
-	while (*p != '\0')
+	while (*str != '\0')
 	{
 		i++;
-		p++;
+		str++;
 	}
 	return (i);
 }
 /**
-* string_nconcat - concatenar
-* @s1: js
-* @s2: sj
-* @n: unsigned
-* Return: restultado de las dos s
+* string_nconcat - a function that concatenates two strings
+* @s1: string one
+* @s2: string two
+* @n: a variable
+* Return: result
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *r;
-	unsigned int a, b;
+	char *rslt;
+	unsigned int idx1, idx2;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	r = malloc(_strlen(s1) + n + 1 * sizeof(*r));
-	if (!r)
+	rslt = malloc(_strlen(s1) + n + 1 * sizeof(*rslt));
+	if (!rslt)
 		return (NULL);
-	for (a = 0; s1[a]; a++)
-		r[a] = s1[a];
-	for (b = 0; a < n && s2[b]; b++)
-		r[a + b] = s2[b];
-	r[a + b] = '\0';
-	return (r);
+	for (idx1 = 0; s1[idx1]; idx1++)
+		rslt[idx1] = s1[idx1];
+	for (idx2 = 0; idx2 < n && s2[idx2]; idx2++)
+		rslt[idx1 + idx2] = s2[idx2];
+	rslt[idx1 + idx2] = '\0';
+	return (rslt);
 }
