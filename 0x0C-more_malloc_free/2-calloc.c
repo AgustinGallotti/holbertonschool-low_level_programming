1b15eae0bbe95d03ffe59ba0dd665c1d4cee5af1
AgustinGallotti <Agustingallotti23@gmail.com>
2022-03-03 10:15:01 -0800

    commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,15 +1,41 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
+
 /**
-*
-*
+* _memset -  a function that fills memory with a constant byte
+* @s: pointer
+* @b: constant
+* @n: n times
+* Return: pointer to the memory area
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n;)
+	{
+		s[a] = b;
+		a++;
+	}
+	return (s);
+}
+/**
 *
 *
 *
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ar;
 
-	int a;
+	ar = malloc(size * nmemb);
 
-	a = malloc(sizeofar)
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	if (!ar)
+		return (NULL);
+	if (ar)
+		_memset(ar, 0, size * nmemb);
+	return (ar);
+}
