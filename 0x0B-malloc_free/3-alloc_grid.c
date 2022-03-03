9f79d204b6b9521788e95ccf6affdeb13749b906
AgustinGallotti <Agustingallotti23@gmail.com>
2022-03-03 04:41:12 -0800

    commit

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*
+*
+*
+*
+*/
+int **alloc_grid(int width, int height);
+{
+	int *ptr;
+	int a = 0;
+
+	ptr = malloc (sizeof(int *ptr) * width)
+	{
+	for (a = 0; a < width; a++)
+	{
+		ptr[a] = malloc (sizeof(int *ptr) * height)
+	}
+	for (a = 0; a < height; a++)
+	{
+		free (ptr[i]);
+		free (ptr);
+	}
+	return (0);
+}
