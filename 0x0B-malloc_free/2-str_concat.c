f34377264fab22284fb601d5ee33a3c94fe52f29
AgustinGallotti <Agustingallotti23@gmail.com>
2022-03-02 17:06:04 -0800

    commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -29,14 +29,9 @@ char *str_concat(char *s1, char *s2)
 int a, b, c, i;
 char *concate;
 
-if (!s1)
-{
-	return ('\0');
-}
-if (!s2)
-{
-	return ('\0');
-}
+if (!s1 == 0)
+
+if (!s2 == 0)
 
 a = _strlen(s1);
 b = _strlen(s2);
