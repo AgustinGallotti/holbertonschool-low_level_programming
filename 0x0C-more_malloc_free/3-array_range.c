80f60f83ab7a6fca1ae9e3004fb24a8252cfcbf8
AgustinGallotti <Agustingallotti23@gmail.com>
2022-03-03 11:26:06 -0800

    commit

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -3,14 +3,23 @@
 #include <stdlib.h>
 
 /**
-*
-*
-*
-*
-*
+* array_range - rango de minimos a maximo
+* @min: nuestro punto mas bajo
+* @max: nuestro punto mas alto
+* Return: a de un nuevo array
 */
-int *array_range(int min, int max);
+int *array_range(int min, int max)
+{
+	int t;
+	int b;
+	int *a;
 
-	int a;
-
-
+	if (min > max)
+		return (NULL);
+	a = malloc((max - min) + 1 * sizeof(int));
+		if (!a)
+		return (NULL);
+	for (t = min; t <= max; t++)
+		a[b++] = t;
+	return (a);
+}
