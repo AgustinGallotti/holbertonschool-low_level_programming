712ac3d465a01dde40a5504fb69ef30086721805
AgustinGallotti <Agustingallotti23@gmail.com>
2022-03-09 11:24:56 -0800

    arreglos de betty

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,12 +10,12 @@
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	long unsigned int i;
+	unsigned long int i;
 
 	if (array == NULL || action == NULL)
 	{
 		return;
 	}
 	for (i = 0; i < size; i++)
-		action (array[i]);
+		action(array[i]);
 }
