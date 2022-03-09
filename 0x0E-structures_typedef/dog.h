7e55e42a19f37d91093f38a8787004013df86744
AgustinGallotti <Agustingallotti23@gmail.com>
2022-03-08 17:12:36 -0800

    Doooog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,10 @@
 #ifndef DOG_H
 #define DOG_H
-/*
-* dog - Define a new type struct dog with the following elements
-* Name: Poppy
-* Age: 3.5
-* Owner: Bob
+/**
+* struct dog - Define a new type struct dog with the following elements
+* @name: Poppy
+* @age: 3.5
+* @owner: Bob
 */
 typedef struct dog
 {
