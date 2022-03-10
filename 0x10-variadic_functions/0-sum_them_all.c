75fc3d9f6cccab13fc64be9efef129072a4ee287
AgustinGallotti <Agustingallotti23@gmail.com>
2022-03-10 10:51:06 -0800

    betty repair

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,10 +11,10 @@ int sum_them_all(const unsigned int n, ...)
 	va_list leer;
 	int sum = 0;
 	unsigned int m = 0;
-	
+
 	if (n == 0)
 	{
-		return(0);
+		return (0);
 	}
 	va_start(leer, n);
 	for (m = 0; m < n; m++)
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "varidic_functions.h"
+/**
+*
+*
+*
+*
+*/
+char c;
+int i;
+float f;
+char *s;
+
