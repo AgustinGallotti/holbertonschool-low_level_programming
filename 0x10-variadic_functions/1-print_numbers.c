df08548633e2a40cc8a8fcdf9a9f202dde549c77
AgustinGallotti <Agustingallotti23@gmail.com>
2022-03-10 06:02:18 -0800

    commit

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+*
+*
+*
+*
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	
+	int x = 0;
+	char = re;
+
+	if (separator == NULL)
+		return ("Error")
+	if (n == NULL)
+		printf("nil");
+	va_start
+	for (x = 0; x < n; x++)
+		printf("%d\n", p);
