e21a6fbcb77b660bf40a8947730796d140479f53
AgustinGallotti <Agustingallotti23@gmail.com>
2022-03-11 15:59:47 -0800

    recorrec

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,56 +1,91 @@
 #include <stdio.h>
-#include "varidic_functions.h"
+#include "variadic_functions.h"
 /**
-* print_all - printeamos todos los argumentos
-* @c: chars
-* @i: integrales
-* @f: floats
-* @s: puntero
+* _strlen  -  returns the length of a string
+* @s: string
+* Return: len
+*/
+int _strlen(char *s)
+{
+	size_t strlen(const char *s);
+	return (strlen(s));
+}
+/**
+* print_all - a function that prints anything
+* @format: list of types of arguments passed to the function
 */
 void print_all(const char * const format, ...)
 {
-	int i = 0;
-	int r = 0;
+	va_list list;
+	size_t i = 0;
+	int j = 0;
+	char *separator = "";
 
-	equ f_array[] = {
-		{'s', print_char*},
-    	{'c', print_char},
-    	{'i', print_inte},
-    	{'f', print_float};
-		{NULL, NULL};
-  	};
+	typef typel[] = {
+			{"c", print_char},
+			{"i", print_integer},
+			{"f", print_float},
+			{"s", print_charchar},
+			{"/0", NULL}
+	};
 
-va_list equ
-{
-	while (r < equ)
+	va_start(list, format);
+	while (format != NULL && format[i] != '\0')
 	{
-		while (format.equ == array)
-		r++;
+		j = 0;
+		while (typel[j].argument != NULL)
+		{
+			if (*typel[j].argument == format[i])
+			{
+				printf("%s", separator);
+				typel[j].f(list);
+				separator = ", ";
+			}
+			j++;
+		}
+		i++;
 	}
-	if (*f_array[i].id == format[r]);
-			*f_array[i].f(equ);
-void print_ch(va_list equ)
-{
-	if (!s)
-		printf("(nil)");
-		printf("%s", print_ch);
-}
-
-void print_float(va_list equ)
-{
-		printf("%f", f);
-}
-
-void print_char(va_list equ)
-{
-		printf("%c", c);
-}
-
-void print_
-{
-		printf("%c", c);
-	{
 	printf("\n");
 	va_end(list);
-	}
 }
+/**
+* print_char - print a char
+* @list: list
+*/
+void print_char(va_list list)
+{
+	printf("%c", va_arg(list, int));
+}
+/**
+* print_integer - print integer
+* @list: list
+*/
+void print_integer(va_list list)
+{
+	printf("%d", va_arg(list, int));
+}
+/**
+* print_float - print a float
+* @list: list
+*/
+void print_float(va_list list)
+{
+	printf("%f", va_arg(list, double));
+}
+/**
+* print_charchar - print a string
+* @list: list
+*/
+void print_charchar(va_list list)
+{
+	char *va;
+
+	va = va_arg(list, char *);
+	if (va == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", va);
+}
+
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,33 +1,31 @@
-#ifndef HAA
-#define HAA
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stdlib.h>
 #include <stdio.h>
-#include <stdarg>
-/**
-*
-*
-*
-*
-*
-*/
-{
+#include <string.h>
+
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
 void print_char(va_list list);
-void print_ch(va_list list);
-void print_inter(va_list list);
+void print_integer(va_list list);
 void print_float(va_list list);
-}
+void print_charchar(va_list list);
 
 /**
-*
-*
-*
-*
+* struct type - a function that prints anything
+* @argument: list
+* @f: pointer
+* Return: anything
 */
-typedef structure list
+
+typedef struct type
 {
-  void (f*)(va_list list);
-  char id;
-} equ;
-#endif(list)
+char *argument;
+void (*f)(va_list list);
+} typef;
+#endif
