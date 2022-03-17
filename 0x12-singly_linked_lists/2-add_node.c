7c8df76fd8c6a646ec26c60d81665365b470544c
AgustinGallotti <Agustingallotti23@gmail.com>
2022-03-17 16:59:02 -0700

    fixing errors x3

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,22 +10,19 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *str1;
 
-	while (head != NULL)
-	{
-		if (!str)
-			return (NULL);
-		
-		str1 = malloc(sizeof(list_t));
+	if (!str)
+		return (NULL);
+	
+	str1 = malloc(sizeof(list_t));
 		
 
-		if (!str1)
-				return (NULL);
+	if (!str1)
+			return (NULL);
 		
-		str1->str = strdup(str);
-		str1->len = strlen(str);
-		str1->next = *head;
-		*head = str1;
+	str1->str = strdup(str);
+	str1->len = strlen(str);
+	str1->next = *head;
+	*head = str1;
 		
-	}
 	return (str1);	
 }
