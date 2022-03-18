36a15a7ccd72283d74ca5101a332a00006631335
AgustinGallotti <Agustingallotti23@gmail.com>
2022-03-17 18:11:51 -0700

    too much long

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,6 +5,7 @@
 */
 int printt(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n
+					I bore my house upon my back!\n");
 	return (0);
 }
