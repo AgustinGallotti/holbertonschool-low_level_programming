1926ff2dc454c51228b8f6575d598ca443ab24f2
AgustinGallotti <Agustingallotti23@gmail.com>
2022-03-17 18:14:08 -0700

    pizza is heree

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@
 */
 int printt(void)
 {
-	printf("You're beat! and yet, you must allow,\n
-					I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 	return (0);
 }
