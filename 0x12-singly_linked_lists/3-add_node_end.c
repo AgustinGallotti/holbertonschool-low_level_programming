24ddb837a5c752a6f010aed9cf475941f08f35bf
AgustinGallotti <Agustingallotti23@gmail.com>
2022-03-17 17:13:55 -0700

    if, else

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -15,13 +15,16 @@ list_t *add_node_end(list_t **head, const char *str)
     str1 = malloc(sizeof(list_t));
 
     if (!str1)
+	{
         return (NULL);
-
-    str1->str = strdup(str);
-    str1->len = strlen(str);
-	*head =	NULL;
-	str1->next = *head;
-    *head = str1;
-
+	}
+	else
+	{
+		str1->str = strdup(str);
+		str1->len = strlen(str);
+		*head =	NULL;
+		str1->next = *head;
+		*head = str1;
+	}
     return (str1);
 }
