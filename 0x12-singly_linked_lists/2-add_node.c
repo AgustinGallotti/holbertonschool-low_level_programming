bcea993f6f684f04938d51d3bf5d7250faecaf53
AgustinGallotti <Agustingallotti23@gmail.com>
2022-03-17 17:00:56 -0700

    Betty fixed

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,7 +2,7 @@
 #include "lists.h"
 /**
 * add_node - function that add a new node to the list_t
-* @head: head of the list_t 
+* @head: head of the list_t
 * @str: new node
 * Return: adress of the new element (str1)
 */
@@ -12,17 +12,16 @@ list_t *add_node(list_t **head, const char *str)
 
 	if (!str)
 		return (NULL);
-	
+
 	str1 = malloc(sizeof(list_t));
-		
 
 	if (!str1)
-			return (NULL);
-		
+		return (NULL);
+
 	str1->str = strdup(str);
 	str1->len = strlen(str);
 	str1->next = *head;
 	*head = str1;
-		
-	return (str1);	
+
+	return (str1);
 }
