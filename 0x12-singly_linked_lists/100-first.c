695640dafcf2afa084d3e68ed13dfc583a14418f
AgustinGallotti <Agustingallotti23@gmail.com>
2022-03-17 18:10:45 -0700

    quote

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+* printt - printt
+* Return: succes
+*/
+int printt(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	return (0);
+}
