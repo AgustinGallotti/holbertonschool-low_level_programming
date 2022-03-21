b175c8ddd7961f2f8c704452b7ef43fb61e92048
AgustinGallotti <Agustingallotti23@gmail.com>
2022-03-21 10:21:56 -0700

    whitout casting show

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* add_nodeint - add node int
+* @listint_t: lista
+* @head: pointer to pointer
+* @n: var
+* Return: counter i
+*/
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *r;
+
+	r = malloc(sizeof(listint_t));
+
+	if (!r)
+	{
+		return (NULL);
+	}
+	r->n = n;
+	r->next = *head;
+	*head = r;
+	return (r);
+}
