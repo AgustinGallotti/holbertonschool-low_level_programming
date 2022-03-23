83423a3611ff5b57f61b9df649187272fa84197a
AgustinGallotti <Agustingallotti23@gmail.com>
2022-03-22 18:52:45 -0700

    nine check

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,23 +1,44 @@
 #include "lists.h"
 /**
-* sum_listint - a function that returns the sum of all the data n of a
-* listint_t linked list
-* @head: Header
-* Return: 0 if the list is NULL
+* insert_nodeint_at_index - a function that inserts a new node at a given
+* position
+* @head: Header of the nodes
+* @idx: is the index of the list where the new node should be added and
+* starts at 0
+* @n: Data of the new node
+* Return: NULL if it is no possible to add the new node at idx
 */
-int sum_listint(listint_t *head)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *current;
-	int data = 0;
+	listint_t *new, *current;
 
-	if (head == NULL)
-		return (0);
+	current = *head;
+	new = malloc(sizeof(listint_t));
 
-	current = head;
-	while (current != NULL)
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+
+	if (idx == 0)
 	{
-		data = data + current->n;
-		current = current->next;
+		new->next = current;
+		*head = new;
+		return (*head);
 	}
-	return (data);
+
+	while (idx > 1)
+	{
+		current = current->next;
+		idx--;
+
+		if (current == NULL)
+		{
+			free(new);
+			return (NULL);
+		}
+	}
+	new->next = current->next;
+	current->next = new;
+	return (new);
 }
