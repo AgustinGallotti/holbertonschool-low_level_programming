20191891ad39059d214d337d097dbf49ed19aff9
AgustinGallotti <Agustingallotti23@gmail.com>
2022-03-22 18:26:43 -0700

    read

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+* sum_listint - a function that returns the sum of all the data n of a
+* listint_t linked list
+* @head: Header
+* Return: 0 if the list is NULL
+*/
+int sum_listint(listint_t *head)
+{
+	listint_t *current;
+	int data = 0;
+
+	if (head == NULL)
+		return (0);
+
+	current = head;
+	while (current != NULL)
+	{
+		data = data + current->n;
+		current = current->next;
+	}
+	return (data);
+}
