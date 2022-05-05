e5586b2489469ee33567f0f0565c74a834fa2d7c
AgustinGallotti <Agustingallotti23@gmail.com>
2022-05-05 11:38:46 -0700

    creect betty for task five

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,16 +1,16 @@
 #include "lists.h"
-/*
+/**
 * get_dnodeint_at_index - find n'th
-*
+* @head: head
+* @index: index
 * Return: new node at idx
 */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *current;
 	unsigned int count = 0;
-	
-	current = head;
 
+	current = head;
 	if (current == NULL)
 		return (NULL);
 	while (current != NULL)
