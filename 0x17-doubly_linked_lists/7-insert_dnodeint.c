b7e8150650ea5f225093a7a8307882bf2ec742a2
AgustinGallotti <Agustingallotti23@gmail.com>
2022-05-05 12:10:06 -0700

    output check2

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		new->next = current;
 		*h = new;
-		return (add_dnodeint(h->n));
+		return (add_dnodeint(h, n));
 	}
 	while (idx > 1)
 	{
@@ -27,7 +27,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		if (current == NULL)
 		{
 			free(new);
-			return (add_dnodeint_end(h->n));
+			return (add_dnodeint_end(h, n));
 		}
 	}
 	new->next = current->next;
