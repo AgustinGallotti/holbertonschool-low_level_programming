4f0d7b98fe062698e2ab46d023cad78a300b2617
AgustinGallotti <Agustingallotti23@gmail.com>
2022-05-05 11:40:32 -0700

    creect betty for task six

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,13 +1,14 @@
 #include "lists.h"
-/*
+/**
 * sum_dlistint - sum of lists
-*
+* @head: head
 * Return: sum of the data
 */
 int sum_dlistint(dlistint_t *head)
 {
 	dlistint_t *current;
 	int data = 0;
+
 	if (head == NULL)
 		return (0);
 	current = head;
