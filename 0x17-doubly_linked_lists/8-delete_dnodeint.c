ccf0b44ddb9708353860e36072a0a26d34d9817a
AgustinGallotti <Agustingallotti23@gmail.com>
2022-05-05 15:38:25 -0700

    dumped solved

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -7,32 +7,44 @@
 */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *current, *next_node;
+	dlistint_t *previous, *next_node, *current = *head;
 	unsigned int i = 0;
 
 	if (head == NULL || *head == NULL)
 		return (-1);
 
-	current = *head;
-
 	if (index == 0)
 	{
-		*head = (*head)->next;
-		current->next = NULL;
+		if ((*head)->next != NULL)
+		{
+			(*head)->next->prev = NULL;
+			*head = (*head)->next;
+		}
+		else
+			*head = NULL;
 		free(current);
 		return (1);
 	}
-
-	while (i < (index - 1))
+	while (i < index)
 	{
 		current = current->next;
 
 		if (current == NULL)
+		{
 			return (-1);
+		}
 		i++;
 	}
+	if (current->next == NULL)
+	{
+		current->prev->next = NULL;
+		free(current);
+		return (1);
+	}
 	next_node = current->next;
-	current->next = next_node->next;
-	free(next_node);
+	previous = current->prev;
+	previous->next = next_node;
+	next_node->prev = previous;
+	free(current);
 	return (1);
 }
