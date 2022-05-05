77cc170029a2888138c71dc1ba5f6ae40f75dac4
AgustinGallotti <Agustingallotti23@gmail.com>
2022-05-05 11:27:22 -0700

    betty two correct

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -19,7 +19,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	new->n = n;
 	new->prev = NULL;
-	new-> next = *head;
+	new->next = *head;
 	*head = new;
 	if (new->next != NULL)
 		(new->next)->prev = new;
