df115f13d9afefdc2150969517bd81f688332da5
AgustinGallotti <Agustingallotti23@gmail.com>
2022-05-05 11:35:08 -0700

    betty four correct

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,11 +1,12 @@
 #include "lists.h"
 /**
-* free-dlistint - free all nodes
-* @header: head
+* free_dlistint - free all nodes
+* @head: head
 */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *current, *nextnode;
+
 	current = head;
 	while (current != NULL)
 	{
