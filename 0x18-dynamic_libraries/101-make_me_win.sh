016e2e1a2f8659743866b480652276b60bf9dfe2
AgustinGallotti <Agustingallotti23@gmail.com>
2022-05-09 11:05:15 -0700

    task

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /root/win.so https://github.com/AgustinGallotti/holbertonschool-low_level_programming/tree/main/0x18-dynamic_libraries/win.so
-export LD_PRELOAD=/root/win.so
+export LD_PRELOAD=/root/gm
+unset LD_PRELOAD
