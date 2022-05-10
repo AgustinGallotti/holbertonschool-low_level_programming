e088eda48510b424e87c32b1807d750482e5dded
AgustinGallotti <Agustingallotti23@gmail.com>
2022-05-09 19:24:58 -0700

    checlomg

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /home/win.so https://github.com/AgustinGallotti/holbertonschool-low_level_programming/tree/main/0x18-dynamic_libraries/win.so
+wget -P /home https://github.com/AgustinGallotti/holbertonschool-low_level_programming/tree/main/0x18-dynamic_libraries/win.so
 export LD_PRELOAD=/home/win.so
