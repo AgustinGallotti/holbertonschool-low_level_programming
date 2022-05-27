3364eebe43b40861d9d6f326bcf9a2634f831fd6
AgustinGallotti <Agustingallotti23@gmail.com>
2022-05-27 15:20:00 -0700

    fied return of strcmp

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,7 +12,7 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *pointer;
-	unsigned long int index;
+	unsigned long int index = 0;
 
 	if (ht == NULL || ht->size == 0 || key == NULL || *key == 0)
 		return (0); /*program checks*/
