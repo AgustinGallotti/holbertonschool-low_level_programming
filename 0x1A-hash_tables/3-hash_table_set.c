d85b1f0d072c7ce4b89802638a3289a757c95929
AgustinGallotti <Agustingallotti23@gmail.com>
2022-05-27 16:38:36 -0700

    test

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,49 +10,40 @@
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
+	unsigned long int index = 0;
 	hash_node_t *newnode, *pointer;
-	char *newvalue;
 
 	if (key == NULL || *key == 0 || ht == NULL || ht->array == NULL
 		|| ht->size == 0)
 		return (0);
 	index = key_index((const unsigned char *) key, ht->size);
-	pointer = ht->array[index];
+	newnode = malloc(sizeof(hash_node_t));
+	if (newnode == NULL)
+	{
+		return (0);
+	}
+	if (newnode->key == NULL)
+	{
+		free(newnode);
+		return (0);
+	}
 	while (pointer != NULL)
 	{
 		if (strcmp(pointer->key, key) == 0)
-			break;
+		{
+			free(pointer->value);
+			pointer->value = strdup(value);
+			free(newnode);
+			return (1);
+		}
+
 		pointer = pointer->next;
 	}
-	if (pointer == NULL)
-	{
-		newnode = malloc(sizeof(hash_node_t));
-		if (newnode == NULL)
-			return (0);
-		newnode->key = strdup(key);
-		if (newnode->key == NULL)
-		{
-			free(newnode);
-			return (0);
-		}
-		newnode->value = strdup(value);
-		if (newnode->value == NULL)
-		{
-			free(newnode->key);
-			free(newnode);
-			return (0);
-		}
-		newnode->next = ht->array[index];
-		ht->array[index] = newnode;
-	}
-	else
-	{
-		newvalue = strdup(value);
-		if (newvalue == NULL)
-			return (0);
-		free(pointer->value);
-		pointer->value = newvalue;
-	}
+
+	newnode->key = strdup(key);
+	newnode->value = strdup(value);
+	newnode->next = ht->array[index];
+	ht->array[index] = newnode;
+
 	return (1);
 }
