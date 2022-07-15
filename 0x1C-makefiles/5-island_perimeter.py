83751276854bc6c0c6d91d69293f026eb15c959a
AgustinGallotti <Agustingallotti23@gmail.com>
2022-07-14 19:25:09 -0700

    check the perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""calculate the perimeter of a grid island"""
+
+
+def island_perimeter(grid):
+    """return the perimeter"""
+    perimeter = 0;
+    r, c = len(grid), len(grid[0])
+    for i in range(r):
+        for j in range(c):
+            if grid[i][j] == 0: continue
+            perimeter += 4;
+            if i > 0: perimeter -= grid[i - 1][j]
+            if j > 0: perimeter -= grid[i][j - 1]
+            if i < r - 1: perimeter -= grid[i + 1][j]
+            if j < c - 1: perimeter -= grid[i][j + 1]
+    return perimeter
