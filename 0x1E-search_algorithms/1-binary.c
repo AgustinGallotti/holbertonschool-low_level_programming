405356bb6b31302784a25759e37f7a77b3c5f957
AgustinGallotti <AgustinGallotti>
2022-08-26 08:23:47 -0700

    no print

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+* binary_search - search for a value in a sorted array of integers
+* @value: value in the array sorted
+*
+* Return: values
+*/
+int binary_search(int *array, size_t size, int value)
+{
+	size_t low = size - 1, hig = 0, mid = 0;
+
+	if (array == NULL)
+		return (-1);
+	else
+	{
+	printf("Searching in array: ");
+		BSH(array, hig, low);
+		mid = (low + hig) / 2;
+		if (array[mid] == value)
+			return (mid);
+	if (value > array[mid])
+		hig = mid + 1;
+	else
+		low = mid - 1;
+	}
+	return (-1);
+}
+/**
+* BSH - print the bin
+* @array: pointer
+* @hig: right index
+* @low: left index
+*/
+void BSH(int *array, size_t hig, size_t low)
+{
+	size_t i;
+
+	for (i = low; i <= hig; i++)
+	{
+		if (i != hig)
+			printf("%d, ", array[i]);
+		else
+			printf("%d\n", array[i]);
+	}
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,1 +1,3 @@
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void BSH(int *array, size_t hig, size_t low);
