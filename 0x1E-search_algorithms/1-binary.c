8ee0ac1597e3c60dd322cc49a62e0328a9ba8fb3
AgustinGallotti <AgustinGallotti>
2022-08-26 08:27:23 -0700

    negative for search in right is the correct, noprint solved

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -14,7 +14,7 @@ int binary_search(int *array, size_t size, int value)
 
 	if (array == NULL)
 		return (-1);
-	else
+	while (low <= hig)
 	{
 	printf("Searching in array: ");
 		BSH(array, hig, low);
